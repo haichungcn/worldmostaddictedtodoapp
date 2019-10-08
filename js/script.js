55a99dcb10984ef371119b46988ecc0e2e3e8c25
haichungcn <haichungcn@gmail.com>
2019-10-08 11:29:44 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const toDos = [
+  {
+    user : "Anonymous",
+    tasks : [
+      {
+        content : "grab food",
+        isDone : false,
+        dateCreated : "",
+        dateDone : "",
+        note : "",
+        piority : 1
+      }
+    ],
+    doneList : []
+  }
+]
+
+function addTask() {
+  
+}
